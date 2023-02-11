2397e7466e1efa86732c287a6a4d7ed7c52deae8
Joe S <joexstrickland@gmail.com>
2023-02-11 16:03:46 -0500

    Added electricity cost mapping to reports. TODO Refactor multi-use of electric logic.

--- controllers/newController.js
+++ controllers/newController.js
@@ -9,14 +9,15 @@
 
 const reportModel = require("../models/report");
 const reportMapping = require("../controllers/reportMapping");
+const report = require("../models/report");
 
 //Post method for submitting a new report to the database
 exports.submitNew = (req, res, next) => {
 
-    //If the advancement costs are left blank, then renter is there for first 3 months (No charge for prev months of no living there)
-    if(req.body.pest === "0" && req.body.water === "0" && req.body.billing === "0" && req.body.storm === "0" && req.body.trash === "0" && req.body.sewer === "0") {
+    //(No Advancement + No electricity) If the advancement costs are left blank, then renter is there for first 3 months (No charge for prev months of no living there)
+    if(req.body.pest === "0" && req.body.water === "0" && req.body.billing === "0" && req.body.storm === "0" && req.body.trash === "0" && req.body.sewer === "0" && req.body.electricity === "0") {
+
         //These values need to be thrown out, but app needs to retain the other fees. Submit app as normal but do not do any advancement logic
-        console.log("No advancement")
         let submittedReport = new reportModel(req.body);
         submittedReport.total = calcTotal(req.body);
         
@@ -28,12 +29,96 @@ exports.submitNew = (req, res, next) => {
                 res.redirect("/prevReport")
             } catch(e) {
                 console.log("Error while saving...")
-                res.render("error", {error: e});
+                next("error", {error: e});
+            }
+        }
+            
+        saveReport();
+    
+    //(No Advancement + Electricity)
+    } else if(req.body.pest === "0" && req.body.water === "0" && req.body.billing === "0" && req.body.storm === "0" && req.body.trash === "0" && req.body.sewer === "0" && req.body.electricity !== "0") {
+
+        const advElec = req.body.electricity;
+        req.body.electricity = "0";
+
+        let reportDate = reportMapping.mapElecDate(req.body.month, req.body.year);
+
+        async function updateElec() {
+            try{
+                const result = await reportModel.findOneAndUpdate(
+                    {
+                        month: reportDate[0], 
+                        year: reportDate[1]
+                    },
+                    {
+                        electricity : advElec,
+                    },
+                    {
+                        upsert: true,
+                    }
+                );
+            } catch (e) {
+                console.log("Error while updating electricity advancement.");
+                next("error", {error: e});
+            };
+        };
+
+        updateElec();
+
+        async function updateAdvElecTotal() {
+            let updateSum = 0;
+            try {
+                const findResult = await reportModel.findOne({ month: reportDate[0], year: reportDate[1] });
+
+                updateSum += parseFloat(findResult.total); 
+                updateSum += parseFloat(findResult.electricity);
+
+            } catch(e) {
+                console.log("Error while updating electricity advancement total.");
+                next("error", {error: e});
+            }
+
+            try {
+                const result = await reportModel.findOneAndUpdate(
+                    {
+                        month: reportDate[0], 
+                        year: reportDate[1]
+                    },
+                    {
+                        total : updateSum,
+                    },
+                    {
+                        upsert: true,
+                    },
+                )
+            } catch(e) {
+                console.log("Error while updating electricity advacement total.");
+                next("error", {error: e});
+            };
+
+        };
+
+        updateAdvElecTotal();
+
+        let submittedReport = new reportModel(req.body);
+
+        submittedReport.total = calcTotal(req.body);
+        
+        //Async function to await the saving functionality, catch errors
+        async function saveReport() {
+            try {
+                const result = await submittedReport.save();
+                console.log(result)
+                res.redirect("/prevReport")
+            } catch(e) {
+                console.log("Error while saving...")
+                next("error", {error: e});
             }
         }
             
         saveReport();
 
+    //(Advancement + Electricity)
     } else {
         console.log("advancement");
         //Seperate out the advancements to place in a previous report. Set the current month's advancements to 0 as they are not known.
@@ -41,8 +126,76 @@ exports.submitNew = (req, res, next) => {
         setAdvDefault(req.body);
 
         //Electricity is a monthly cost, seperate out the electricity submission.
-        const electricity = req.body.electricity;
+        const advElec = req.body.electricity;
         req.body.electricity = "0";
+
+        let reportDateElec = reportMapping.mapElecDate(req.body.month, req.body.year);
+
+        //If the default was triggered, something has gone really wrong.
+        if(reportDateElec[0] === "ERROR" || reportDateElec[1] === "ERROR") {
+            console.log("Advancement Electric Date mapping error. Default triggered.")
+            let mapError = new Error("Invalid month entered for report. Please try again.");
+            mapError.status = 400;
+            return next(mapError);
+        }
+
+
+        async function updateElec() {
+            try{
+                const result = await reportModel.findOneAndUpdate(
+                    {
+                        month: reportDateElec[0], 
+                        year: reportDateElec[1]
+                    },
+                    {
+                        electricity : advElec,
+                    },
+                    {
+                        upsert: true,
+                    }
+                );
+            } catch (e) {
+                console.log("Error while updating electricity advancement.");
+                return next("error", {error: e});
+            };
+        };
+
+        updateElec();
+
+        async function updateAdvElecTotal() {
+            let updateSum = 0;
+            try {
+                const findResult = await reportModel.findOne({ month: reportDateElec[0], year: reportDateElec[1] });
+
+                updateSum += parseFloat(findResult.total); 
+                updateSum += parseFloat(findResult.electricity);
+
+            } catch(e) {
+                console.log("Error while updating electricity advancement total.");
+                return next("error", {error: e});
+            }
+
+            try {
+                const result = await reportModel.findOneAndUpdate(
+                    {
+                        month: reportDateElec[0], 
+                        year: reportDateElec[1]
+                    },
+                    {
+                        total : updateSum,
+                    },
+                    {
+                        upsert: true,
+                    },
+                )
+            } catch(e) {
+                console.log("Error while updating electricity advacement total.");
+                return next("error", {error: e});
+            };
+
+        };
+
+        updateAdvElecTotal();
 
         //Determine the month and year the advancement costs must go to. [Month, Year]
         let reportDate = reportMapping.mapAdvDate(req.body.month, req.body.year);
@@ -83,7 +236,7 @@ exports.submitNew = (req, res, next) => {
 
             } catch(e) {
                 console.log("Error while updating advancements...");
-                res.render("error", {error: e});
+                return next("error", {error: e});
             }
         }
 
@@ -105,7 +258,7 @@ exports.submitNew = (req, res, next) => {
 
             } catch(e) {
                 console.log("Error when updating advancement total...");
-                res.render("error", {error: e});
+                return next("error", {error: e});
             }
 
             console.log(updateSum);
@@ -125,10 +278,9 @@ exports.submitNew = (req, res, next) => {
                 );
                 
                 console.log("Total updated.")
-                console.log(updateResult);
             } catch(e) {
                 console.log("Error when updating advancement total...");
-                res.render("error", {error: e});
+                return next("error", {error: e});
             }
 
         }
@@ -147,7 +299,7 @@ exports.submitNew = (req, res, next) => {
                 res.redirect("/prevReport")
             } catch(e) {
                 console.log("Error while saving...")
-                res.render("error", {error: e});
+                return next("error", {error: e});
             }
         }
             
@@ -179,7 +331,6 @@ const setAdvDefault = (body) => {
 }
 
 //Deriving the total summation of values from req.body. (For submitNew REST method)
-//TODO exclude certain mapping costs, Electricity pays for 2 months prior, advancement costs are for previous months
 const calcTotal = (body) => {
     const values = Object.values(body)
     const disVals = []
--- controllers/reportMapping.js
+++ controllers/reportMapping.js
@@ -6,7 +6,7 @@
 
 */
 
-//Mapping the report months
+//Mapping the advancement costs --> month, year
 exports.mapAdvDate = (repMonth, repYear) => {
     let mapMonth = "";
     let mapYear = "";
@@ -58,6 +58,69 @@ exports.mapAdvDate = (repMonth, repYear) => {
             break;
         case "December":
             mapMonth = "September";
+            mapYear = repYear;
+            break;
+        case "Default":
+            mapMonth = "ERROR";
+            mapYear = "ERROR";
+            break;
+    }
+
+    return [mapMonth, mapYear];
+};
+
+//Mapping the electric cost --> month, year
+exports.mapElecDate = (repMonth, repYear) => {
+    let mapMonth = "";
+    let mapYear = "";
+
+    switch(repMonth) {
+        case "January":
+            mapMonth = "December";
+            mapYear = (parseInt(repYear) - 1).toString();
+            break;
+        case "February":
+            mapMonth = "January";
+            mapYear = repYear;
+            break;
+        case "March":
+            mapMonth = "February";
+            mapYear = repYear;
+            break;
+        case "April":
+            mapMonth = "March";
+            mapYear = repYear;
+            break;
+        case "May":
+            mapMonth = "April";
+            mapYear = repYear;
+            break;
+        case "June":
+            mapMonth = "May";
+            mapYear = repYear;
+            break;
+        case "July":
+            mapMonth = "June";
+            mapYear = repYear;
+            break;
+        case "August":
+            mapMonth = "July";
+            mapYear = repYear;
+            break;
+        case "September":
+            mapMonth = "August";
+            mapYear = repYear;
+            break;
+        case "October":
+            mapMonth = "September";
+            mapYear = repYear;
+            break;
+        case "November":
+            mapMonth = "October";
+            mapYear = repYear;
+            break;
+        case "December":
+            mapMonth = "November";
             mapYear = repYear;
             break;
         case "Default":
