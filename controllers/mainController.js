d50552a6d89e4d30d56ea44619d8a8fb9798cf73
Joe S <joexstrickland@gmail.com>
2023-04-02 16:59:23 -0400

    Created dynamic thumbnail loading page

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -22,6 +22,24 @@ exports.getNewReport = (req, res, next) => {
 //GET /prevReport page
 exports.getPrevReport = (req, res, next) => {
     //Grab the computer's current year. Display all 12 months from that year
+    //let currentYear = 2022;
+    let currentYear = new Date().getFullYear();
 
-    res.render("viewReport");
+    async function getReportsForYear() {
+        try {
+            const result = await reportModel.find({ year: currentYear });
+            let existingMonths = [];
+            result.forEach(report => {
+                existingMonths.push(report.month)    
+            });
+            
+            res.render("viewReports", {currentYear, existingMonths, result});
+        } catch(e) {
+            console.log("Error when collecting reports...");
+            next("error", {error: e});
+        };
+    };
+
+    getReportsForYear();
+
 }
